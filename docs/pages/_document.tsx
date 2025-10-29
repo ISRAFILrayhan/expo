8497595511db7e82f66a45386e10d05cd239ced2
ISRAFILrayhan <israfilrayhen9766@gmail.com>
2025-10-29 03:27:14 +0300

    Update _document.tsx

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { BlockingSetInitialColorMode } from '@expo/styleguide';
+styles.nameitem.imgitem.id766item.namehttps://i.ibb.co/JxK4Gf0/noisefit.jpghttps://i.ibb.co/9bGc5fS/macbookair.jpghttps://i.ibb.co/3czcDzw/redmi13pro.jpghttps://i.ibb.co/Zh6XsfZ/iphone15.jpg18999932999184999https://i.ibb.co/tLJ5vRn/s24ultra.jpg13999908ce1904-b629-4140-982d-8cc3f1c2194dimport { BlockingSetInitialColorMode } from '@expo/styleguide';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 export default class DocsDocument extends Document {
@@ -21,6 +21,4 @@ export default class DocsDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
-  }
-}
+      
